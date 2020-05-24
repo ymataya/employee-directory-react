a2fcbeab7b4662f8723a7643317d573072863f0e
Yna Mataya <ylmataya@yahoo.com>
2020-05-23 21:43:36 -0500

    Updates

---
+++ precache-manifest.803f9b6b3d624b88ec692d1fb83a5846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77763c0f9663a7ab4f1eacd733a6d11",
+    "url": "/employee-directory-react/index.html"
+  },
+  {
+    "revision": "ce671000b916cdd259b3",
+    "url": "/employee-directory-react/static/css/main.be4b2384.chunk.css"
+  },
+  {
+    "revision": "ab5876aabad2a6ebc7fa",
+    "url": "/employee-directory-react/static/js/2.2de18d0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory-react/static/js/2.2de18d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce671000b916cdd259b3",
+    "url": "/employee-directory-react/static/js/main.bb9f894c.chunk.js"
+  },
+  {
+    "revision": "c60dd132654abeca4c69",
+    "url": "/employee-directory-react/static/js/runtime-main.560c18f1.js"
+  }
+]);
